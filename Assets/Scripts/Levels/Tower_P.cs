35e3c5239860e52b4f43c35dd25b804ea9bc9257
justXDng <justinngo99@gmail.com>
2021-11-11 14:12:33 -0800

    remade tilemap, placeholder background for level 1, added towers, and added rigidbody and 2d collider physics for the towers and the tilemap

---
+++ Assets/Scripts/Levels/Tower_P.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_P : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Object Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
